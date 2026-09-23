ac2ac37b6d67ab56ddc6b62b4383a58e2462dd30
jhonatanchagasacademico@gmail.com <jhonatanchagasacademico@gmail.com>
2026-09-23 13:01:39 -0300

    aula0923-criando User e SignIn

--- services/authService.js
+++
@@ -1,8 +0,0 @@
-import { User } from "../models/User.js";
-import admin from "firebase-admin";
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault()
-  });
-}
